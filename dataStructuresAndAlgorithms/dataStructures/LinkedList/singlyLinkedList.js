24b80dc3c91ce4c629acda1c121513522cd65653
mongare <mongaresams@gmail.com>
2022-10-15 11:49:15 +0300

    Brief summary of what linked lists entail and methods to manipulate the list

---
+++ dataStructuresAndAlgorithms/dataStructures/LinkedList/singlyLinkedList.js
@@ -0,0 +1,11 @@
+// A single linked list is similat to an array but it has some 
+// significant difference which makes it an effective data structure in
+// some circumstances. One of the major differences is that it does not
+// have defined indexed and navigation through the list is through nodes.
+// Singly linked lists also have no predefined sizes like arrays in the
+// initialization face and they grow and reduce based on the size of the
+// list.
+
+// List manipulation methods include:
+// size, head, addLast(push), addFirst(unShift), addAt(insert), removeFirst(shift), 
+// remove(pop), removeAt, indexOf, isEmpty, elementAt, findMiddle, get, clean, rotateListRight
