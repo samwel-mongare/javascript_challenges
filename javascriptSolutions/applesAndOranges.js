613327b69be8c882512ee29182a35b84dcbe0e5c
mongare <mongaresams@gmail.com>
2022-11-04 12:23:12 +0300

    Solve daily challenge on hackerank. Apples and Oranges

---
+++ javascriptSolutions/applesAndOranges.js
@@ -0,0 +1,7 @@
+// https://www.hackerrank.com/challenges/apple-and-orange/problem?isFullScreen=true
+
+// The apple and oranges challenge is a beginner level challenge that basically
+// wanted a calculation of how many apples and oranges fall onto Sam's house from the 
+// respective trees. I solved the challenge using arrays and the for loop
+// JavaScript method. Lesson learnt after research is that an optimized 
+// solution would have been using the map function which produces more DRY code.
