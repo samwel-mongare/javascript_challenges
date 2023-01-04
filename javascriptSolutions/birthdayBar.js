377a61541b3cbfe7aa61e0b6ac9c3a783531537c
mongare <mongaresams@gmail.com>
2023-01-04 06:52:56 +0300

    Add brief explanation of birthday bar challenge

---
+++ javascriptSolutions/birthdayBar.js
@@ -0,0 +1,7 @@
+/*  https://www.hackerrank.com/challenges/the-birthday-bar/problem
+The birthday bar problem exploits the understanding of loops and how to loop
+through an array while suming up the components
+I believe that there is an optimized solution for this problem using a sorting
+algorithm but for now, here is a solution with a big O of O(n2);
+*/
+
