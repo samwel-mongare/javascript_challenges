583431ba3498613fc9578354f441a3f9cf986567
mongare <mongaresams@gmail.com>
2023-01-05 06:53:25 +0300

    Add solution to the challenge

--- javascriptSolutions/birthdayBar.js
+++ javascriptSolutions/birthdayBar.js
@@ -5,3 +5,17 @@ I believe that there is an optimized solution for this problem using a sorting
 algorithm but for now, here is a solution with a big O of O(n2);
 */
 
+function birthday(s, d, m) {
+  // Write your code here
+  let chocolatePieces = 0, sum = 0;
+  for(let i = 0; i<= s.length - m; i++){
+      sum = 0;
+      for(let j = i; j < i+m; j++){
+          sum += s[j];
+      }
+      if(sum == d){
+          chocolatePieces++;
+      }
+  }
+  return chocolatePieces;
+}
