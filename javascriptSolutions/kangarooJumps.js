569026cf390b58a9a2f0fbe611c05d48382e485e
mongare <mongaresams@gmail.com>
2023-01-22 05:30:42 +0300

    Explanation of my thinking process to this algorithm

---
+++ javascriptSolutions/kangarooJumps.js
@@ -0,0 +1,11 @@
+/*
+Link to the challenge: https://www.hackerrank.com/challenges/kangaroo/problem?isFullScreen=true
+The hackerrank challenged requires us to find out if two kangaroos in the circus actually get to meet 
+at a specific position during the show.
+The approach I used in this challenge is I initialized two parameters to store the sum
+of both numbers.
+I then compared the total of both parameters in a while loop with the condition that the two numbers were not
+the same.
+To help with the runtime, I put an upper limit of 10 000 000 000;
+I also added some conditional statements to handle the base case scenarios.
+*/
