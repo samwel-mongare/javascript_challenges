1e3ba5fc001f94ccff7e72819ea6929425f541f7
mongare <mongaresams@gmail.com>
2023-01-23 05:32:47 +0300

    Add solution to the challenge

--- javascriptSolutions/kangarooJumps.js
+++ javascriptSolutions/kangarooJumps.js
@@ -9,3 +9,20 @@ the same.
 To help with the runtime, I put an upper limit of 10 000 000 000;
 I also added some conditional statements to handle the base case scenarios.
 */
+
+function kangaroo(x1, v1, x2, v2) {
+   let kang1 = x1;
+   let kang2 = x2;
+   if(x2 >= x1 && v2 >= v1 || x1 >= x2 && v1 >= v2 || v2 === 0 || v1 === 0) {
+       return "NO";
+   }
+    
+   while(kang1 !== kang2 && kang1 < 100000000) {
+       kang1 = kang1 + v1;
+       kang2 = kang2 + v2;
+   }
+   if(kang1 === kang2) {
+       return "YES"
+   }
+   return "NO"
+}
