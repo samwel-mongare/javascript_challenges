9a539953791977a55d3ba3ab22338c67cee11818
mongare <mongaresams@gmail.com>
2023-02-09 14:06:27 +0300

    Add test data for priority queue and weighted graph

--- dataStructuresAndAlgorithms/dataStructures/dijkstrasAlgorithm.js
+++ dataStructuresAndAlgorithms/dataStructures/dijkstrasAlgorithm.js
@@ -4,30 +4,42 @@ class WeightedGraph {
   }
 
   addVertex(vertex) {
-    if(!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+    if(!this.adjacencyList[vertex]) this.adjacencyList[vertex] = []; 
   }
 
   addEdge(vertex1, vertex2, weight) {
-    this.adjacencyList[vertex1].push({ node:vertex2, weight});
-    this.adjacencyList[vertex2].push({ node:vertex1, weight});
+    this.adjacencyList[vertex1].push({ node: vertex2, weight});
+    this.adjacencyList[vertex2].push({ node: vertex1, weight});
   }
 }
 
-class priorityQueue {
+class PriorityQueue {
   constructor() {
     this.values = [];
   }
 
   enqueue(val, priority) {
-    this.values.push({ val, priority });
-    this.sort()
+    this.values.push({ val, priority});
+    this.sort();
   }
 
   dequeue() {
-    return this.values.shift();
+    return this.values.shift()
   }
 
   sort() {
-    this.values.sort((a,b) => a.priority - b.priority)
+    this.values.sort((a,b) => a.priority - b.priority);
   }
 }
+
+// const graph = new WeightedGraph();
+// graph.addVertex("A");
+// graph.addVertex("B");
+// graph.addVertex("C");
+// graph.addVertex("D");
+
+// graph.addEdge("A", "B", 3);
+// graph.addEdge("A", "C", 4);
+// graph.addEdge("B", "D", 2);
+// graph.addEdge("B", "C", 1);
+// graph.addEdge("C", "D", 1);
