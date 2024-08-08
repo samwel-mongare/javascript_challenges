fc223b813f98b2d548db70751705f5653a17aff2
mongare <mongaresams@gmail.com>
2024-08-08 09:48:53 +0300

    Create bank account

---
+++ coltSteele/javascriptClasses.js
@@ -0,0 +1,8 @@
+// Bank implementation >>>>>>>>>>>>
+class BankAccount {
+  Constructor(accountHolder, accountNumber, balance) {
+    this.accountHolder = accountHolder;
+    this.accountNumber = accountNumber;
+    this.balance = balance ? balance : 0;
+  }
+}
