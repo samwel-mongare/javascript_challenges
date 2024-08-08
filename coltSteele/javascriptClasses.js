c08e1ff51fb0fe3967efcb21e8727fd8ca81496b
mongare <mongaresams@gmail.com>
2024-08-08 09:49:31 +0300

    Add Bank deposit

--- coltSteele/javascriptClasses.js
+++ coltSteele/javascriptClasses.js
@@ -5,4 +5,17 @@ class BankAccount {
     this.accountNumber = accountNumber;
     this.balance = balance ? balance : 0;
   }
+
+  deposit = () => {
+    const accountToDeposit = prompt(
+      `Please input the account number to deposit ${this.accountNumber}`
+    );
+    const amount = prompt("Please input the amount to deposit");
+
+    if (accountToDeposit === this.accountNumber) {
+      this.balance += amount;
+      return alert(`You new account balance is Ksh.${this.balance}`);
+    }
+    return alert("Please input a valid account Number!!!");
+  };
 }
