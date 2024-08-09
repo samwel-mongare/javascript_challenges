9de525ec2e6f7901eb947f2c3fbc79ade01afcff
mongare <mongaresams@gmail.com>
2024-08-09 09:49:16 +0300

    Add Bank withdrawal

--- coltSteele/javascriptClasses.js
+++ coltSteele/javascriptClasses.js
@@ -18,4 +18,25 @@ class BankAccount {
     }
     return alert("Please input a valid account Number!!!");
   };
+
+  withdraw = () => {
+    const accountToWithdraw = prompt(
+      "Please input the account number to withdraw"
+    );
+    const amount = prompt("Please input the amount to withdraw");
+
+    if (
+      accountToWithdraw === this.accountNumber &&
+      this.balance >= this.amount
+    ) {
+      this.balance -= amount;
+      return alert(`You new account balance is Ksh.${this.balance}`);
+    }
+    if (this.amount > this.balance) {
+      return prompt(
+        `Insufficient funds in the account please input a value less or equal to ${this.balance}`
+      );
+    }
+    return alert("Please input a valid account Number!!!");
+  };
 }
